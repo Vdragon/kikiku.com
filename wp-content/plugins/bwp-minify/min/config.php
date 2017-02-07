25aa3db649884b7e3c225bee457d3bc76f403d66
LiuZhi <yezige@foxmail.com>
2017-02-07 02:49:29 -0700

    minify

--- wp-content/plugins/bwp-minify/min/config.php
+++ wp-content/plugins/bwp-minify/min/config.php
@@ -7,10 +7,10 @@ $min_cachePath = dirname(dirname(__FILE__)) . '/cache';
 $min_documentRoot = '';
 $min_cacheFileLocking = true;
 $min_serveOptions['bubbleCssImports'] = true;
-$min_serveOptions['maxAge'] = 86400;
+$min_serveOptions['maxAge'] = 259200;
 $min_serveOptions['minApp']['groupsOnly'] = false;
 $min_symlinks = array();
 $min_uploaderHoursBehind = 0;
 $min_libPath = dirname(__FILE__) . '/lib';
 ini_set('zlib.output_compression', '0');
-// auto-generated on 2014-10-20 18:27:24
+// auto-generated on 2017-02-07 01:44:34
